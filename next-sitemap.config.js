88302b9a5d407f7053f0334ab891fe1bd6b20b5b
Rody Castro <therodycc@gmail.com>
2025-05-22 09:16:45 -0400

    Uploading sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.epagriculard.com/',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'weekly',
+    priority: 0.7,
+};
