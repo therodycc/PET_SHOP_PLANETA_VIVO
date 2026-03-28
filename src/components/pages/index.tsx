658b96a0e502c3bd2981647255f7fb52e6bc54aa
Rody Castro <therodycc@gmail.com>
2026-03-28 12:29:53 -0400

    Feat: Loading page

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,18 +1,22 @@
-"use client"
+"use client";
+import { useEffect } from "react";
 import { Header } from "../layout/header/header";
 import { Footer } from "./home/footer";
 import { Mvv } from "./home/mvv/mvv";
 import { NetworkSection } from "./home/network/network";
 import { OurServices } from "./home/our-services";
+import { LoadingPage } from "./loading";
 
 export const HomeIndex = () => {
   return (
-    <>
-      <Header />
-      <OurServices />
-      <Mvv />
-      <NetworkSection />
-      <Footer />
-    </>
+    <LoadingPage>
+      <>
+        <Header />
+        <OurServices />
+        <Mvv />
+        <NetworkSection />
+        <Footer />
+      </>
+    </LoadingPage>
   );
 };
--- src/components/pages/loading/index.tsx
+++ src/components/pages/loading/index.tsx
@@ -1,53 +1,55 @@
-import { CSSProperties, useEffect, useState } from "react";
+import { CSSProperties, ReactNode, useEffect, useState } from "react";
 
-export const LoadingPage = () => {
-    const [animationEnd, setAnimationEnd] = useState(false);
-    const [loading, setLoading] = useState(true);
-    const [finished, setFinished] = useState(false);
+export const LoadingPage = ({ children }: { children: ReactNode }) => {
+  const [animationEnd, setAnimationEnd] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [finished, setFinished] = useState(false);
 
-    useEffect(() => {
-        const loadingTimer = setTimeout(() => {
-            setLoading(false);
-            clearTimeout(loadingTimer);
-        }, 1000);
-    }, []);
+  useEffect(() => {
+    const loadingTimer = setTimeout(() => {
+      setLoading(false);
+      clearTimeout(loadingTimer);
+    }, 0);
+  }, []);
 
-    useEffect(() => {
-        if (!loading) {
-            setAnimationEnd(true);
-            const timer = setTimeout(() => {
-                setFinished(true);
-                clearTimeout(timer)
-            }, 800);
-        }
-    }, [loading, setFinished]);
+  useEffect(() => {
+    if (!loading) {
+      setAnimationEnd(true);
+      const timer = setTimeout(() => {
+        setFinished(true);
+        clearTimeout(timer);
+      }, 600);
+    }
+  }, [loading, setFinished]);
 
-    return !finished ? (
-        <div
-            className={`d-flex justify-content-center align-items-center  ${animationEnd ? "animate__animated animate__fadeOut" : ""
-                }`}
-            style={styles.bg}
-        >
-            <div className="spinner">
-                <div className="rect1"></div>
-                <div className="rect2"></div>
-                <div className="rect3"></div>
-                <div className="rect4"></div>
-                <div className="rect5"></div>
-            </div>
-        </div>
-    ) : null;
+  return !finished ? (
+    <div
+      className={`d-flex justify-content-center align-items-center  ${
+        animationEnd ? "animate__animated animate__fadeOut" : ""
+      }`}
+      style={styles.bg}
+    >
+      <div className="spinner">
+        <div className="rect1"></div>
+        <div className="rect2"></div>
+        <div className="rect3"></div>
+        <div className="rect4"></div>
+        <div className="rect5"></div>
+      </div>
+    </div>
+  ) : (
+    children
+  );
 };
 
-
 const styles: { [key: string]: CSSProperties } = {
-    bg: {
-        backgroundColor: "#008374",
-        height: "100vh",
-        position: "fixed",
-        width: "100%",
-        top: 0,
-        left: 0,
-        zIndex: 99909,
-    }
-}
+  bg: {
+    backgroundColor: "#008374",
+    height: "100vh",
+    position: "fixed",
+    width: "100%",
+    top: 0,
+    left: 0,
+    zIndex: 99909,
+  },
+};
